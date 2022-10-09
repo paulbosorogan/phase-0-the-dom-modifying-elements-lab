0c6ca127e7157385ee860af1e974e4aca4ac88fa
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-09 13:52:11 -0400

    commit tree bc290cd5bcb02cffc8658ad8e3799a00d84259a1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElim = document.querySelector('main')
+mainElim.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Paul is the champion'
